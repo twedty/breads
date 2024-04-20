0967f9c4df79bc8607a282d86119ddf6106b76f2
twedty <jamestwedt@gmail.com>
2024-04-20 13:25:05 -0400

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -3,9 +3,17 @@ const breads = express.Router()
 const Bread = require('../models/bread.js')
 
 // INDEX
+// INDEX
 breads.get('/', (req, res) => {
-    res.send(Bread)
+    res.render('Index',
+        {
+        breads: Bread
+        }
+    )
+  // res.send(Bread)
 })
+
+
 
 module.exports = breads
 
